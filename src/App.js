fb0fc89b39100f747b903b0d83d437058b68b13f
Nick Sardonini <nsardonini@gmail.com>
2021-02-12 13:53:52 -0500

    import & add Board component to App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import styled from "styled-components";
-import Nav from "./components/Nav";
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
-
-const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
-`;
+import Board from "./components/Board";
 
 function App() {
   return (
     <>
-      <Wrapper>
-        <Nav />
-      </Wrapper>
+      <Board />
     </>
   )
 }
