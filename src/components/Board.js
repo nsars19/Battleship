d6219c81384555998976f902e3162356c873bd2a
Nick Sardonini <nsardonini@gmail.com>
2021-02-12 13:54:21 -0500

    add Board component

---
+++ src/components/Board.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import { useState } from "react";
+
+const StyledBoard = styled.div``;
+
+const Board = (props) => {
+  const [grid, setGrid] = useState(createBoard());
+
+  console.log(grid);
+
+  return <StyledBoard>Hello</StyledBoard>;
+};
+
+function createBoard() {
+  let arr = [];
+
+  for (let i = 0; i < 10; i++) {
+    arr[i] = [[], [], [], [], [], [], [], [], [], []];
+  }
+
+  return arr;
+}
+
+export default Board;
