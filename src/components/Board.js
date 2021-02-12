c0132cd4ff1e28627deea6f0bdd060184d3ef89e
Nick Sardonini <nsardonini@gmail.com>
2021-02-12 14:55:27 -0500

    add testing styles to Board

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,7 +2,14 @@ import styled from "styled-components";
 import { useState } from "react";
 import Tile from "./Tile";
 
-const StyledBoard = styled.div``;
+const StyledBoard = styled.div`
+  height: 400px;
+  width: 400px;
+  border: 2px solid #0004;
+  display: grid;
+  grid-template-columns: repeat(10, minmax(20px, 50px));
+  grid-template-rows: repeat(10, minmax(20px, 50px));
+`;
 
 const Board = (props) => {
   const [grid, setGrid] = useState(createBoard());
