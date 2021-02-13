b22d74ccfa5dc7b71359e3cd04b358e0731c7705
Nick Sardonini <nsardonini@gmail.com>
2021-02-12 19:37:43 -0500

    import & add global styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GlobalStyle from "./globalStyles";
 import App from './App';
+import styled from "styled-components";
+
+const StyledApp = styled(App)`
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+`;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <StyledApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
