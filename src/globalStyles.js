2b397b491bd1d8af089b89a0ddc50792fe5c9974
Nick Sardonini <nsardonini@gmail.com>
2021-02-12 19:37:25 -0500

    add global stylesheet

---
+++ src/globalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+    background-color: #f0f0f0;
+    box-sizing: border-box;
+    font-family: Courier, Arial, Sans-Serif;
+  }
+`;
+
+export default GlobalStyle;
