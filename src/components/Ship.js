b4a40ea990c8ed48f0eef2a0279bfb17f806d995
Nick Sardonini <nsardonini@gmail.com>
2021-02-12 19:44:03 -0500

    style Ship component

--- src/components/Ship.js
+++ src/components/Ship.js
@@ -1,5 +1,19 @@
+import styled from "styled-components";
+
+const StyledShip = styled.div`
+  height: ${(props) => 40 * parseInt(props.length)}px;
+  width: 40px;
+  background: #222;
+`;
+
 const Ship = (props) => {
-  return <div></div>;
+  const handleDrag = (e) => {
+    console.log(e);
+  };
+
+  return (
+    <StyledShip length={props.length} draggable={true} onDrag={handleDrag} />
+  );
 };
 
 export default Ship;
