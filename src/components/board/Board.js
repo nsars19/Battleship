184e54f186b5a26aab728993fc29520dea168dd1
Nick Sardonini <nsardonini@gmail.com>
2021-02-17 17:47:38 -0500

    add shipId to modifyGrid function, set value to grid tile

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -5,9 +5,9 @@ import StyledBoard from "./StyledBoard";
 const Board = (props) => {
   const [grid, setGrid] = useState(createBoard());
 
-  const modifyGrid = (x, y) => {
+  const modifyGrid = (x, y, id) => {
     let updatedGrid = grid;
-    updatedGrid[x][y] = "";
+    updatedGrid[x][y] = id;
 
     setGrid(updatedGrid);
   };
@@ -15,6 +15,7 @@ const Board = (props) => {
   const placeShip = (e, direction = "vertical") => {
     const [x, y] = e.target.id.split("");
     const length = parseInt(e.dataTransfer.getData("length"));
+    const shipId = parseInt(e.dataTransfer.getData("id"));
 
     switch (direction) {
       case "horizontal":
@@ -23,12 +24,12 @@ const Board = (props) => {
           
           if (tile) {
             tile.style.background = "#222";
-            modifyGrid(x - i, y);
+            modifyGrid(x - i, y, shipId);
           } else {
             let diff = length - i;
             tile = document.getElementById(`${parseInt(x) + diff}${y}`);
             tile.style.background = "#222";
-            modifyGrid(parseInt(x) + diff, y);
+            modifyGrid(parseInt(x) + diff, y, shipId);
           }
         }
         break;
@@ -39,12 +40,12 @@ const Board = (props) => {
           
           if (tile) {
             tile.style.background = "#222";
-            modifyGrid(x, y - i);
+            modifyGrid(x, y - i, shipId);
           } else {
             let diff = length - i;
             tile = document.getElementById(`${x}${parseInt(y) + diff}`);
             tile.style.background = "#222";
-            modifyGrid(x, parseInt(y) + diff);
+            modifyGrid(x, parseInt(y) + diff, shipId);
           }
         }
         break;
