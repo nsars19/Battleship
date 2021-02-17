adc3f264c51ca0fdadc81a7c6e948a03314c9d6f
Nick Sardonini <nsardonini@gmail.com>
2021-02-17 17:13:11 -0500

    move styled component to it's own module

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,22 +1,13 @@
-import styled from "styled-components";
 import { useState } from "react";
 import Tile from "../tile/Tile";
-
-const StyledBoard = styled.div`
-  height: 400px;
-  width: 400px;
-  border: 2px solid #0004;
-  display: grid;
-  grid-template-columns: repeat(10, minmax(20px, 50px));
-  grid-template-rows: repeat(10, minmax(20px, 50px));
-`;
+import StyledBoard from "./StyledBoard";
 
 const Board = (props) => {
   const [grid, setGrid] = useState(createBoard());
 
   const modifyGrid = (x, y) => {
     let updatedGrid = grid;
-    updatedGrid[x][y] = "X";
+    updatedGrid[x][y] = "";
 
     setGrid(updatedGrid);
   };
