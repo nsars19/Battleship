dd022f823951d6e3892f18e0cecf3ef140f965a5
Nick Sardonini <nsardonini@gmail.com>
2021-02-19 16:42:00 -0500

    add cpu tile choosing method & helper functions for that method

--- src/factories/player/Player.js
+++ src/factories/player/Player.js
@@ -13,7 +13,99 @@ const Player = (cpu = false) => {
     return true;
   };
 
-  const cpuAttack = (coord, board) => {};
+  const generateRandomTile = () => {
+    console.log("random tile");
+  };
+
+  function getHits(attacks, ships, hits = []) {
+    if (attacks.length === 0) return hits;
+
+    const attack = attacks.pop();
+    let ship = ships.filter((ship) => ship.hitCoords().includes(attack))[0];
+
+    if (ship) hits.push(attack);
+    return getHits(attacks, ships, hits);
+  }
+
+  const multiDimensionalUnique = (arr) => {
+    let uniques = [];
+    let itemsFound = {};
+    for (let i = 0, l = arr.length; i < l; i++) {
+      let stringified = JSON.stringify(arr[i]);
+      if (itemsFound[stringified]) {
+        continue;
+      }
+      uniques.push(arr[i]);
+      itemsFound[stringified] = true;
+    }
+    return uniques;
+  };
+
+  const generateTileGuesses = (board) => {
+    const attacks = Object.assign([], attackList);
+    const ships = board.getShips();
+    const hits = getHits(attacks, ships);
+    const mostRecentHit = hits[0];
+    const ship = ships.filter((ship) =>
+      ship.hitCoords().includes(mostRecentHit)
+    )[0];
+
+    if (!mostRecentHit) {
+      generateRandomTile();
+    } else {
+      let choices = [];
+      hits.sort((firstHit, secondHit) => {
+        const [x, y] = firstHit;
+        const [a, b] = secondHit;
+
+        if (x === a) {
+          // Vertical ships
+          choices.push(
+            ...new Set([
+              [x, y + 1],
+              [x, y - 1],
+              [a, b + 1],
+              [a, b - 1],
+            ])
+          );
+        }
+        if (y === b) {
+          // Horizontal ships
+          choices.push(
+            ...new Set([
+              [x + 1, y],
+              [x - 1, y],
+              [a + 1, b],
+              [a - 1, b],
+            ])
+          );
+        }
+      });
+
+      const uniqueChoices = multiDimensionalUnique(choices).filter(
+        (coord) => !hits.map((i) => i.join("")).includes(coord.join(""))
+      );
+
+      if (uniqueChoices.length !== 0) {
+        // Filter out previous hits
+        return uniqueChoices;
+      } else {
+        const [x, y] = hits[0];
+        choices.push([x + 1, y], [x - 1, y], [x, y + 1], [x, y - 1]);
+        const filteredChoices = choices.filter((coord) => {
+          return (
+            coord[0] >= 0 && coord[1] >= 0 && coord[0] < 10 && coord[1] < 10
+          );
+        });
+
+        return filteredChoices.sort();
+      }
+    }
+  };
+
+  const cpuAttack = (coord, board) => {
+    playerAttack(coord, board);
+  };
 
   const attack = (coord, board) => {
     return cpu ? cpuAttack(coord, board) : playerAttack(coord, board);
@@ -23,6 +115,7 @@ const Player = (cpu = false) => {
     attack,
     attackList,
     board: GameBoard(),
+    generateTileGuesses,
   };
 };
 
