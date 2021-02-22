0244585cfee68041fa840bc60198fdc0d687a0b7
Nick Sardonini <nsardonini@gmail.com>
2021-02-22 16:25:47 -0500

    add setter method for ship object coordinates
    adds setCoords, which clears the current coordinates in the coords variable, and assigns
    the variable the value of the argument passed to it. This arg should be a two dimensional array of coordinates

--- src/factories/ship/Ship.js
+++ src/factories/ship/Ship.js
@@ -16,9 +16,15 @@ const ShipFactory = (coordsArr) => {
     return a.every((coord) => b.includes(coord));
   };
 
+  const setCoords = (newCoords) => {
+    while (coords.length !== 0) coords.pop();
+    coords.push(...newCoords);
+  };
+
   return {
     coords,
     isSunk,
+    setCoords,
     length: () => coords.length,
     hit: (hitCoord) => hitCoords.push(hitCoord),
     hitCoords: () => hitCoords,
