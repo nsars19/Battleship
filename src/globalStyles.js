3ee26a8cc0956802098fee30e7e242ea7d2ed082
Nick Sardonini <nsardonini@gmail.com>
2021-02-24 17:09:55 -0500

    add button and image default styles, change app styled to use wildcard

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,12 +1,40 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  body,
+  * {
     padding: 0;
     margin: 0;
-    background-color: #f0f0f0;
+    background-color: #222831;
     box-sizing: border-box;
     font-family: Courier, Arial, Sans-Serif;
+  }
+
+  button {
+    margin: 10px;
+    padding: 7px;
+    border: 0 solid transparent;
+    border-radius: 2px;
+    background-color: #393e46;
+    color: #eee;
+    letter-spacing: 1.4px;
+    transition: all 0.1s ease-in-out;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    &:hover {
+      background-color: #424149;
+    }
+    &:active {
+      background-color: #46454a;
+    }
+  }
+
+  img {
+    height: 32px;
+    width: 32px;
+    background: transparent;
   }
 `;
 
