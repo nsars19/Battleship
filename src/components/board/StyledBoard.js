917ca9f19e21b682efd4eb37e5e048938f0b456d
Nick Sardonini <nsardonini@gmail.com>
2021-02-25 18:59:08 -0500

    remove border, add media queries for responsiveness

--- src/components/board/StyledBoard.js
+++ src/components/board/StyledBoard.js
@@ -1,12 +1,26 @@
 import styled from "styled-components";
 
 const StyledBoard = styled.div`
-  height: 400px;
-  width: 400px;
-  border: 2px solid #0004;
+  height: 280px;
+  width: 280px;
+  margin: 15px auto;
   display: grid;
-  grid-template-columns: repeat(10, minmax(20px, 50px));
-  grid-template-rows: repeat(10, minmax(20px, 50px));
+  grid-template-columns: repeat(10, minmax(5px, 1fr));
+  grid-template-rows: repeat(10, minmax(5px, 1fr));
+
+  @media (min-width: 540px) {
+    & {
+      width: 400px;
+      height: 400px;
+    }
+  }
+
+  @media (min-width: 768px) {
+    & {
+      width: 500px;
+      height: 500px;
+    }
+  }
 `;
 
 export default StyledBoard;
